de6e5ab8a4f9ca102b5f2107497cf4695cd03fb2
madhurajayashanka <madhuraweerasooriye@gmail.com>
2022-08-02 09:22:32 +0530

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,46 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
+import { PostListComponent } from './posts/post-list/post-list.component';
+import {MatExpansionModule} from "@angular/material/expansion";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {MatCardModule} from "@angular/material/card";
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatInputModule} from "@angular/material/input";
+import {MatIconModule} from "@angular/material/icon";
+import {AuthInterceptor} from "./auth/auth.interceptor";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PostListComponent,
+    PostCreateComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatExpansionModule,
+        HttpClientModule,
+        MatCardModule,
+        MatFormFieldModule,
+        ReactiveFormsModule,
+        MatInputModule,
+        MatIconModule,
+        FormsModule
+    ],
+  providers: [
+    {provide:HTTP_INTERCEPTORS,useClass:AuthInterceptor,multi:true},
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
